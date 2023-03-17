74e3b033d8120bfc5cd4cf26b184f69843f7a8d7
辛雨坤 <@>
2023-03-17 14:27:46 +0800

    添加项目文件。

---
+++ 1328Xin Yukun WindowsFormsApp1/XmlTextReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml;
+
+namespace _1328Xin_Yukun_WindowsFormsApp1
+{
+    internal class XmlTextReader
+    {
+        private string v;
+
+        public XmlTextReader(string v)
+        {
+            this.v = v;
+        }
+
+        public XmlNodeType NodeType { get; internal set; }
+        public bool HasAttributes { get; internal set; }
+        public string Value { get; internal set; }
+        public string Name { get; internal set; }
+
+        internal void MoveToAttribute(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MoveToElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MoveToFirstAttribute()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MoveToNextAttribute(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MoveToNextAttribute()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ReadString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
